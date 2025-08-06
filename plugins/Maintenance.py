26910376616412058affeb8d61df26e57d8e3b41
Rockers2 <151193983+Rockers2@users.noreply.github.com>
2025-08-06 22:52:16 +0530

    Update Maintenance.py

--- plugins/Maintenance.py
+++ plugins/Maintenance.py
@@ -11,7 +11,7 @@ async def maintenance_checker(client, message: Message):
     chat_id = message.chat.id
     if MAINTENANCE_MODE and user_id not in BYPASS_IDS and chat_id not in BYPASS_IDS:
         await message.reply(
-            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nSupport Group: @AV_SUPPORT_GROUP",
+            "🚧 **Bot is under Maintenance!**\n\nPlease try again later.\n\nContact : @knmlpro2_contactbot",
             quote=True
         )
         return
