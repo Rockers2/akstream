e5652264567acfe41edb360a49fbbefec20cf313
Rockers2 <151193983+Rockers2@users.noreply.github.com>
2025-08-13 06:32:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import os, sys, glob, pytz, asyncio, logging, importlib
 from pathlib import Path
 from pyrogram import idle
+from aiohttp import web
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
+# Dont Remove My Credit @AV_BOTz_UPDATE
+# This Repo Is By @BOT_OWNER26
 # For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 logging.basicConfig(
@@ -13,20 +14,15 @@ logging.basicConfig(
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
- 
+
 from info import *
 from typing import Union, Optional, AsyncGenerator
-from Script import script 
-from datetime import date, datetime 
-from aiohttp import web
+from Script import script
+from datetime import date, datetime
 from web import web_server, check_expired_premium
 from web.server import Webavbot
 from utils import temp, ping_server
 from web.server.clients import initialize_clients
-
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
@@ -35,7 +31,7 @@ loop = asyncio.get_event_loop()
 
 async def start():
     print('\n')
-    print('Initalizing Your Bot')
+    print('Initializing Your Bot')
     bot_info = await Webavbot.get_me()
     await initialize_clients()
     for name in files:
@@ -50,12 +46,9 @@ async def start():
             sys.modules["plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-    
     if ON_HEROKU:
         asyncio.create_task(ping_server())
+
     me = await Webavbot.get_me()
     temp.BOT = Webavbot
     temp.ME = me.id
@@ -66,18 +59,24 @@ async def start():
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
     Webavbot.loop.create_task(check_expired_premium(Webavbot))
-    await Webavbot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-    await Webavbot.send_message(chat_id=ADMINS[0] ,text='<b>ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
-    await Webavbot.send_message(chat_id=SUPPORT_GROUP, text=f"<b>{me.mention} ʀᴇsᴛᴀʀᴛᴇᴅ 🤖</b>")
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-    await idle()
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+    await Webavbot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    await Webavbot.send_message(chat_id=ADMINS[0], text='<b>ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !!</b>')
+    await Webavbot.send_message(chat_id=SUPPORT_GROUP, text=f"<b>{me.mention} ʀᴇsᴛᴀʀᴛᴇᴅ 🤖</b>")
+
+    # --- Add Keep-Alive Ping Route ---
+    app = await web_server()
+    async def ping(request):
+        return web.Response(text="Pong")
+    app.router.add_get('/ping', ping)
+    # ---------------------------------
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(runner, bind_address, PORT).start()
+
+    await idle()
 
 if __name__ == '__main__':
     try:
